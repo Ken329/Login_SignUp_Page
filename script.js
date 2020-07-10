b7afef88645ce1c06eb9a24123365dc41d450bbc
Ken329 <67051443+Ken329@users.noreply.github.com>
2020-07-10 19:39:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var attempts = 3;
+var myuser = "kenliau";
+var mypass = "kahsoon";
+
+document.getElementById("mybtn").addEventListener("click", check);
+
+function check(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if(username == myuser && password == mypass){
+		//alert("Successful login");
+		window.location = "success.html";
+		return false;
+	}else{
+		attempts--;
+		alert(attempts + " attempts left");
+		if (attempts == 0) {
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("mybtn").disabled = true;
+			return false;
+		}
+	}	
+}
+
+
